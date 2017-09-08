72aa5c87766be4900abb9d8e9b78db6032f3c1f9
kiyokyle <30451559+kiyokyle@users.noreply.github.com>
2017-09-07 18:06:09 -0700

    Files for Login and database

--- Smart City Project/SmartCityProject/SmartCityProject/settings.py
+++ Smart City Project/SmartCityProject/SmartCityProject/settings.py
@@ -76,7 +76,11 @@ WSGI_APPLICATION = 'SmartCityProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'SmartCitydatabase',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
