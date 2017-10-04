c686694f09b1ca310c35c202598c60794cb76c9d
kiyokyle <30451559+kiyokyle@users.noreply.github.com>
2017-10-04 10:01:51 -0700

    Added First Name Last Name

--- Smart City Project/SmartCityProject/regpage/forms.py
+++ Smart City Project/SmartCityProject/regpage/forms.py
@@ -6,10 +6,13 @@ from django.contrib.auth.models import User, Group
 class regpage(UserCreationForm):
     email = forms.EmailField(max_length = 100, required= True)
     groups = forms.ModelChoiceField(queryset=Group.objects.all(), label="Account Type", required= True)
+    first_name = forms.CharField(max_length= 30, required= False)
+    last_name = forms.CharField(max_length= 30, required= False)
+    address = forms.CharField(max_length= 30, required = False)
 
     class Meta:
         model = User
-        fields = ('username','password1', 'password2','email', 'groups')
+        fields = ('username','password1', 'password2','email', 'groups','first_name', 'last_name', 'address')
         help_texts = {
             'username': "30 characters or less. Characters, digits and @/./+/-/_ only."
         }
--- Smart City Project/SmartCityProject/regpage/models.py
+++ Smart City Project/SmartCityProject/regpage/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class websiteUser(models.Model):
+    user = models.OneToOneField(User)
+    address = models.CharField(max_length= 30)
+    phonenumber = models.CharField(max_length= 30)
